4107ea6a40eb8b1f188230fea8ee53a441e83d03
ddkhen <daniel@whatsfabric.com>
2024-07-28 23:22:14 -0400

    Add firstName, lastName, email, and password fields, remove fullName field

--- src/main/java/com/danielkhen/websocket/user/User.java
+++ src/main/java/com/danielkhen/websocket/user/User.java
@@ -1,6 +1,9 @@
 package com.danielkhen.websocket.user;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -12,15 +15,36 @@ import org.springframework.data.mongodb.core.mapping.Document;
  */
 @Getter
 @Setter
-@Document
+@Document(collection = "users")
 public class User {
     /** Unique identifier for the user, also serves as their nickname */
     @Id
+    @NotBlank
+    @Size(max = 20)
     private String nickName;
 
-    /** The full name of the user */
-    private String fullName;
+    /** The first name of the user */
+    @NotBlank
+    @Size(max = 20)
+    private String firstName;
+
+    /** The last name of the user */
+    @NotBlank
+    @Size(max = 20)
+    private String lastName;
+
+    /** The email of the user */
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    /** The password of the user */
+    @NotBlank
+    @Size(max = 120)
+    private String password;
 
     /** The current status of the user (online/offline) */
+    @NotBlank
     private Status status;
 }
