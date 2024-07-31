9c95960e84871d7e9f531c98c7348fcb413cff12
ddkhen <daniel@whatsfabric.com>
2024-07-30 23:29:35 -0400

    Update User.java w/ app constants

--- src/main/java/com/danielkhen/websocket/user/User.java
+++ src/main/java/com/danielkhen/websocket/user/User.java
@@ -1,6 +1,7 @@
 package com.danielkhen.websocket.user;
 
 
+import com.danielkhen.websocket.util.AppConstants;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,28 +21,28 @@ public class User {
     /** Unique identifier for the user, also serves as their nickname */
     @Id
     @NotBlank
-    @Size(max = 20)
+    @Size(max = AppConstants.MAX_NICKNAME_LENGTH)
     private String nickName;
 
     /** The first name of the user */
     @NotBlank
-    @Size(max = 20)
+    @Size(max = AppConstants.MAX_FIRSTNAME_LENGTH)
     private String firstName;
 
     /** The last name of the user */
     @NotBlank
-    @Size(max = 20)
+    @Size(max = AppConstants.MAX_LASTNAME_LENGTH)
     private String lastName;
 
     /** The email of the user */
     @NotBlank
-    @Size(max = 50)
+    @Size(max = AppConstants.MAX_EMAIL_LENGTH)
     @Email
     private String email;
 
     /** The password of the user */
     @NotBlank
-    @Size(max = 120)
+    @Size(max = AppConstants.MAX_PASSWORD_LENGTH)
     private String password;
 
     /** The current status of the user (online/offline) */
