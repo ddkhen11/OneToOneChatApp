7dc4343d85335ae57b82b0b742e92f396e76132e
ddkhen <daniel@whatsfabric.com>
2024-07-30 23:29:49 -0400

    Update ChatMessage.java w/ app constants

--- src/main/java/com/danielkhen/websocket/chat/ChatMessage.java
+++ src/main/java/com/danielkhen/websocket/chat/ChatMessage.java
@@ -1,5 +1,9 @@
 package com.danielkhen.websocket.chat;
 
+import com.danielkhen.websocket.util.AppConstants;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -22,17 +26,23 @@ public class ChatMessage {
     private String id;
 
     /** Unique identifier for the chat room this message belongs to */
+    @NotBlank
     private String chatId;
 
     /** ID of the user who sent this message */
+    @NotBlank
     private String senderId;
 
     /** ID of the user who should receive this message */
+    @NotBlank
     private String recipientId;
 
     /** Content of the message */
+    @NotBlank
+    @Size(max = AppConstants.MAX_MESSAGE_LENGTH)
     private String content;
 
     /** Timestamp when the message was sent */
+    @NotNull
     private Date timestamp;
 }
