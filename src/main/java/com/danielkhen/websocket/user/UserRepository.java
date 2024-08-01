7751dc467cc291b6d4b1351a2df1fa93ae9dc376
ddkhen <daniel@whatsfabric.com>
2024-07-31 23:54:40 -0400

    Add UserDetailsServiceImpl

--- src/main/java/com/danielkhen/websocket/user/UserRepository.java
+++ src/main/java/com/danielkhen/websocket/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.danielkhen.websocket.user;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByNickName(String nickName);
     List<User> findAllByStatus(Status status);
 }
